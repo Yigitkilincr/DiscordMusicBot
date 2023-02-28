6f7152eed95a136a2e75ca4f8c0f96a4cb431a50
Yiğit Kılınçer <yigitkilincer12@gmail.com>
2023-02-28 13:06:49 +0300

    Beta version for music player that has some major issues about skip command.

---
+++ slash/play.js
@@ -0,0 +1,89 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+const { EmbedBuilder } = require("discord.js")
+const { QueryType } = require("discord-player")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("play")
+		.setDescription("Play songs with keywods")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("url")
+				.setDescription("Plays a song from a url")
+				.addStringOption((option) => option.setName("yoururl").setDescription("the song's url").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("spotify")
+				.setDescription("Plays a song from spotify based on provided keywords")
+				.addStringOption((option) => option.setName("spotifyurl").setDescription("the keywords for song").setRequired(true))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("youtube")
+				.setDescription("Plays a song from youtube based on provided keywords")
+				.addStringOption((option) => option.setName("keywords").setDescription("the keywords for song").setRequired(true))
+		),
+	run: async ({ client, interaction }) => {
+		if (!interaction.member.voice.channel) return interaction.editReply("You need to be in a VC to use this command")
+
+		const queue = await client.player.createQueue(interaction.guild)
+		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
+
+		let embed = new EmbedBuilder()
+
+		if (interaction.options.getSubcommand() === "url") {
+            let url = interaction.options.getString("your url")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_VIDEO
+            })
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results")
+            
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+
+		} else if (interaction.options.getSubcommand() === "spotify") {
+            let url = interaction.options.getString("spotifyurl")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.SPOTIFY_SONG
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results")
+            
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+		} else if (interaction.options.getSubcommand() === "youtube") {
+            let url = interaction.options.getString("keywords")
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_SEARCH
+            })
+
+            if (result.tracks.length === 0)
+                return interaction.editReply("No results")
+            
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
+		}
+        if (!queue.playing) await queue.play()
+        await interaction.editReply({
+            embeds: [embed]
+        })
+	},
+}
